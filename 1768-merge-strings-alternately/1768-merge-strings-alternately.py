85134b611743fa1d45e84ab22ab0c9943ab328fd
Rodel Joseph Sijo <s.rodeljoseph@gmail.com>
2026-07-30 18:48:40 +0530

    Time: 47 ms (45.71%), Space: 19.2 MB (88.07%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -2,10 +2,18 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         result = []
+
+        # Find the length of the shorter word
         min_length = min(len(word1), len(word2))
+
+        # Loop exactly min_length times
         for i in range(min_length):
             result.append(word1[i])
             result.append(word2[i])
+
+        # Slice and append the remaining chunks.
+        # (If a word is out of letters, the slice just returns an empty string "")
         result.append(word1[min_length:])
         result.append(word2[min_length:])
+
         return "".join(result)
