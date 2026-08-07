6db192d924a528c8d1664e7e7d54ba7ba05fc0b6
Rodel Joseph Sijo <s.rodeljoseph@gmail.com>
2026-08-07 19:40:51 +0530

    Time: 4 ms (14.92%), Space: 19.3 MB (87.3%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,22 +1,21 @@
 class Solution:
-    def compress(self, chars: List[str]) -> int:
-        if len(chars) == 1:
-            return 1
-            
-        write = 0  
-        front = 0
-        back = 0   
-        
-        while front < len(chars):
-            while front < len(chars) and chars[front] == chars[back]:
-                front += 1
-            chars[write] = chars[back]
+    def compress(self, chars: list[str]) -> int:
+        read = write = 0
+
+        while read < len(chars):
+            char = chars[read]
+            count = 0
+
+            while read < len(chars) and chars[read] == char:
+                read += 1
+                count += 1
+
+            chars[write] = char
             write += 1
-            count = front - back
+
             if count > 1:
                 for digit in str(count):
                     chars[write] = digit
                     write += 1
-            back = front
-            
+
         return write
