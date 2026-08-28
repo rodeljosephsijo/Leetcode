ecac1e43e8ebd2cc976adaeaf2b5200446956f92
Rodel Joseph Sijo <s.rodeljoseph@gmail.com>
2026-08-28 20:05:25 +0530

    Time: 1 ms (57.41%), Space: 8.5 MB (92.08%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        long long rev = 0;
+        int temp = x;
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        return (rev == x);
+    }
+}; 
